27c1c748508067e30b5dfdb161c3ef00cdf23b7f
Takumi Katase <takumi.katase@devoc.ninja>
2024-09-12 01:37:22 +0900

    configure lualine

--- users/kumico/home.nix
+++ users/kumico/home.nix
@@ -26,6 +26,15 @@
       sha256 = "sha256-s3y8ZuLV00GIhizcK/zqsJOTKecql7Xn3LGYmH7NLsQ=";
     };
   };
+  lualine = pkgs.vimUtils.buildVimPlugin {
+    name = "lualine.nvim";
+    src = pkgs.fetchFromGitHub {
+      owner = "nvim-lualine";
+      repo = "lualine.nvim";
+      rev = "master";
+      sha256 = "sha256-gCm7m96PkZyrgjmt7Efc+NMZKStAq1zr7JRCYOgGDuE=";
+    };
+  };
 in {
   home.username = "kumico";
   home.homeDirectory = "/home/kumico";
@@ -182,152 +191,163 @@ in {
       -- guicolors
       if os.getenv("COLORTERM") ~= nil then
         vim.o.termguicolors = true
-      end
+          end
 
-      vim.o.colorcolumn = "100" -- Highlight 100 character limit
+          vim.o.colorcolumn = "100" -- Highlight 100 character limit
 
-         --------------------------------------------------------
-         -- File types
-         --------------------------------------------------------
+          --------------------------------------------------------
+          -- File types
+          --------------------------------------------------------
+
+          vim.api.nvim_create_autocmd("FileType", {
+              pattern = { "go", "python", "c", "zig", "rust" },
+              callback = function()
+              vim.opt_local.autoindent = true
+              vim.opt_local.smartindent = true
+              vim.opt_local.expandtab = true
+              vim.opt_local.tabstop = 4
+              vim.opt_local.softtabstop = 4
+              vim.opt_local.shiftwidth = 4
+              end,
+              })
 
       vim.api.nvim_create_autocmd("FileType", {
-           pattern = { "go", "python", "c", "zig", "rust" },
-           callback = function()
-             vim.opt_local.autoindent = true
-             vim.opt_local.smartindent = true
-             vim.opt_local.expandtab = true
-             vim.opt_local.tabstop = 4
-             vim.opt_local.softtabstop = 4
-             vim.opt_local.shiftwidth = 4
-           end,
-         })
+        pattern = { "vim", "lua", "sh", "nix" },
+        callback = function()
+        vim.opt_local.autoindent = true
+        vim.opt_local.smartindent = true
+        vim.opt_local.expandtab = true
+        vim.opt_local.tabstop = 2
+        vim.opt_local.softtabstop = 2
+        vim.opt_local.shiftwidth = 2
+        end,
+      })
 
-         vim.api.nvim_create_autocmd("FileType", {
-           pattern = { "vim", "lua", "sh" },
-           callback = function()
-             vim.opt_local.autoindent = true
-             vim.opt_local.smartindent = true
-             vim.opt_local.expandtab = true
-             vim.opt_local.tabstop = 2
-             vim.opt_local.softtabstop = 2
-             vim.opt_local.shiftwidth = 2
-           end,
-         })
+      -- disable automatic comment text at line breaks.
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = '*',
+        callback = function()
+        vim.opt_local.formatoptions:remove { 'r', 'o' }
+        end,
+      })
 
-         -- disable automatic comment text at line breaks.
-         vim.api.nvim_create_autocmd('FileType', {
-           pattern = '*',
-           callback = function()
-             vim.opt_local.formatoptions:remove { 'r', 'o' }
-           end,
-         })
+      --------------------------------------------------------
+      -- Keymaps
+      --------------------------------------------------------
 
-         --------------------------------------------------------
-         -- Keymaps
-         --------------------------------------------------------
+      -- Split window
+      vim.api.nvim_set_keymap('n', 'ss', ':split<Return><C-w>w', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', 'sv', ':vsplit<Return>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', 'st', ':tabnew<Return>', { noremap = true, silent = true })
 
-         -- Split window
-         vim.api.nvim_set_keymap('n', 'ss', ':split<Return><C-w>w', { noremap = true, silent = true })
-         vim.api.nvim_set_keymap('n', 'sv', ':vsplit<Return>', { noremap = true, silent = true })
-         vim.api.nvim_set_keymap('n', 'st', ':tabnew<Return>', { noremap = true, silent = true })
+      -- Remap keys
+      vim.api.nvim_set_keymap('n', '<Left>', '<C-w><<CR>', { noremap = true })
+      vim.api.nvim_set_keymap('n', '<Right>', '<C-w>><CR>', { noremap = true })
+      vim.api.nvim_set_keymap('n', '<Up>', '<C-w>+<CR>', { noremap = true })
+      vim.api.nvim_set_keymap('n', '<Down>', '<C-w>-<CR>', { noremap = true })
+      vim.api.nvim_set_keymap('t', ';;', '<C-\\><C-n>', { noremap = true })
+      vim.g.mapleader = " "
+      vim.api.nvim_set_keymap("n", "<ScrollWheelUp>", "<C-Y>", { noremap = true })
+      vim.api.nvim_set_keymap("n", "<ScrollWheelDown>", "<C-E>", { noremap = true })
 
-         -- Remap keys
-         vim.api.nvim_set_keymap('n', '<Left>', '<C-w><<CR>', { noremap = true })
-         vim.api.nvim_set_keymap('n', '<Right>', '<C-w>><CR>', { noremap = true })
-         vim.api.nvim_set_keymap('n', '<Up>', '<C-w>+<CR>', { noremap = true })
-         vim.api.nvim_set_keymap('n', '<Down>', '<C-w>-<CR>', { noremap = true })
-         vim.api.nvim_set_keymap('t', ';;', '<C-\\><C-n>', { noremap = true })
-         vim.g.mapleader = " "
-         vim.api.nvim_set_keymap("n", "<ScrollWheelUp>", "<C-Y>", { noremap = true })
-         vim.api.nvim_set_keymap("n", "<ScrollWheelDown>", "<C-E>", { noremap = true })
+      -- Move windows
+      vim.api.nvim_set_keymap('n', 'sh', '<C-w>h', { noremap = true })
+      vim.api.nvim_set_keymap('n', 'sk', '<C-w>k', { noremap = true })
+      vim.api.nvim_set_keymap('n', 'sj', '<C-w>j', { noremap = true })
+      vim.api.nvim_set_keymap('n', 'sl', '<C-w>l', { noremap = true })
+      vim.api.nvim_set_keymap('n', '<S-i>', '<C-i>', { noremap = true })
+      vim.api.nvim_set_keymap('n', '<CR><CR>', '<C-w>w', { noremap = true })
 
-         -- Move windows
-         vim.api.nvim_set_keymap('n', 'sh', '<C-w>h', { noremap = true })
-         vim.api.nvim_set_keymap('n', 'sk', '<C-w>k', { noremap = true })
-         vim.api.nvim_set_keymap('n', 'sj', '<C-w>j', { noremap = true })
-         vim.api.nvim_set_keymap('n', 'sl', '<C-w>l', { noremap = true })
-         vim.api.nvim_set_keymap('n', '<S-i>', '<C-i>', { noremap = true })
-         vim.api.nvim_set_keymap('n', '<CR><CR>', '<C-w>w', { noremap = true })
+      -- Switch tabs
+      vim.api.nvim_set_keymap('n', '<S-Tab>', 'gT', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<Tab>', 'gt', { noremap = true, silent = true })
 
-         -- Switch tabs
-         vim.api.nvim_set_keymap('n', '<S-Tab>', 'gT', { noremap = true, silent = true })
-         vim.api.nvim_set_keymap('n', '<Tab>', 'gt', { noremap = true, silent = true })
+      -- quickfix window
+      vim.api.nvim_set_keymap("n", "<C-j>", ":cnext<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "<C-k>", ":cprev<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "<C-c>", ":cclose<CR>", { noremap = true, silent = true })
 
-         -- quickfix window
-         vim.api.nvim_set_keymap("n", "<C-j>", ":cnext<CR>", { noremap = true, silent = true })
-         vim.api.nvim_set_keymap("n", "<C-k>", ":cprev<CR>", { noremap = true, silent = true })
-         vim.api.nvim_set_keymap("n", "<C-c>", ":cclose<CR>", { noremap = true, silent = true })
+      -- Set cmdheight
+      vim.o.cmdheight = 1
 
-         -- Set cmdheight
-         vim.o.cmdheight = 1
+      -- Use Terminal shortcut
+      vim.cmd([[
+          autocmd TermOpen * startinsert
+          function! s:Openterm() abort
+          let w = winwidth(win_getid())
+          let h = winheight(win_getid()) * 2.1
+          if h > w
+          exe 'split'
+          exe 'term'
+          else
+          exe 'vsplit'
+          exe 'term'
+          endif
+          endfunction
+          nmap <silent> tt :<C-u>silent call <SID>Openterm()<CR>
+      ]])
 
-         -- Use Terminal shortcut
-         vim.cmd([[
-           autocmd TermOpen * startinsert
-           function! s:Openterm() abort
-             let w = winwidth(win_getid())
-             let h = winheight(win_getid()) * 2.1
-             if h > w
-               exe 'split'
-               exe 'term'
-             else
-               exe 'vsplit'
-               exe 'term'
-             endif
-           endfunction
-           nmap <silent> tt :<C-u>silent call <SID>Openterm()<CR>
-         ]])
+      -- Clear all buffers
+      function clearBuffers()
+        local buffers = vim.api.nvim_list_bufs()
 
-         -- Clear all buffers
-         function clearBuffers()
-           local buffers = vim.api.nvim_list_bufs()
-
-           for _, buffer in ipairs(buffers) do
-             if vim.api.nvim_buf_get_option(buffer, "modified") then
-               print("Buffer " .. buffer .. "has unsaved changes")
-             else
-               vim.api.nvim_buf_delete(buffer, { force = true })
-             end
-           end
-         end
-
-         vim.api.nvim_set_keymap('n', '<Leader>cl', ':lua clearBuffers()<cr>', { noremap = true, silent = true })
-
-         -- yank current buffer file path
-         vim.api.nvim_set_keymap('n', ';c', ':let @0=expand("%:p")<CR>', { noremap = true, silent = true })
-
-         --------------------------------------------------------
-         -- Plugins
-         --------------------------------------------------------
-
-         -- colorscheme
-         require('lflops').setup({
-             italic = {
-             strings = false,
-               emphasis = false,
-               comments = false,
-               operators = false,
-               folds = false,
-             },
-         })
-         -- colorscheme
-         vim.opt.background = "dark"
-         vim.cmd("colorscheme lflops")
-
-         -- Git
-         gitsigns = require('gitsigns').setup({
-      on_attach = function(bufnr)
-               local gitsigns = require('gitsigns')
-               local function map(mode, l, r, desc)
-                 vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
-               end
-               map('n', '<leader>b', function() gitsigns.blame_line { full = true } end)
+        for _, buffer in ipairs(buffers) do
+          if vim.api.nvim_buf_get_option(buffer, "modified") then
+            print("Buffer " .. buffer .. "has unsaved changes")
+          else
+            vim.api.nvim_buf_delete(buffer, { force = true })
+          end
+        end
       end
-         })
+
+      vim.api.nvim_set_keymap('n', '<Leader>cl', ':lua clearBuffers()<cr>', { noremap = true, silent = true })
+
+      -- yank current buffer file path
+      vim.api.nvim_set_keymap('n', ';c', ':let @0=expand("%:p")<CR>', { noremap = true, silent = true })
+
+      --------------------------------------------------------
+      -- Plugins
+      --------------------------------------------------------
+
+      -- colorscheme
+      require('lflops').setup({
+        italic = {
+          strings = false,
+          emphasis = false,
+          comments = false,
+          operators = false,
+          folds = false,
+        },
+      })
+      -- colorscheme
+      vim.opt.background = "dark"
+      vim.cmd("colorscheme lflops")
+
+      -- Git
+      gitsigns = require('gitsigns').setup({
+        on_attach = function(bufnr)
+          local gitsigns = require('gitsigns')
+          local function map(mode, l, r, desc)
+            vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+          end
+          map('n', '<leader>b', function() gitsigns.blame_line { full = true } end)
+        end
+      })
+
+      -- StatusLine
+      require('lualine').setup {
+        options = {
+          theme = 'auto',
+        },
+      }
 
     '';
-    plugins = with pkgs.vimPlugins; [
-      nvim-treesitter
+    plugins = with pkgs; [
+      vimPlugins.nvim-treesitter
+      vimPlugins.nvim-web-devicons
+
+      # custom plugins
+      lualine
       lflops
       gitsigns
     ];
