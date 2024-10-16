153d2b1c47e594b8b5374409f38d188e5ca18472
Takumi Katase <takumi.katase@devoc.ninja>
2024-10-17 02:01:52 +0900

    update kitty background color

--- users/kumico/neovim/lua/plugins/colorscheme.lua
+++ users/kumico/neovim/lua/plugins/colorscheme.lua
@@ -20,6 +20,17 @@ return {
 -- return {
 --   dir = '/home/kumico/go/src/github.com/devoc09/lflops.nvim',
 --   config = function()
---     vim.cmd('')
+--       require('lflops').setup({
+--         italic = {
+--           strings = false,
+--           emphasis = false,
+--           comments = false,
+--           operators = false,
+--           folds = false,
+--         },
+--       })
+--       -- colorscheme
+--       vim.opt.background = "dark"
+--       vim.cmd("colorscheme lflops")
 --   end,
 -- }
--- users/kumico/neovim/lua/plugins/treesitter.lua
+++ users/kumico/neovim/lua/plugins/treesitter.lua
@@ -8,13 +8,15 @@ return {
 	config = function()
 		local configs = require("nvim-treesitter.configs")
 		configs.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "go", "zig", "rust", "python", "yaml", "nix", "make", "gitcommit", "gitignore", "git_rebase", "bash", "fish" },
 			highlight = {
 				enable = true,
 			},
 			endwise = {
 				enable = true,
 			},
+      indent = {
+        enable = true,
+      },
 		})
 	end,
 }
